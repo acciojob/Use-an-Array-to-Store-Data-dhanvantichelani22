53c9f551f5753e91f17bb98fdff872d9e44cb569
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-03-10 15:05:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,2 @@
-//let yourArray = ["Im Dhanvanti",22,true,"Welcome",false];
-//console.log(yourArray.length);
-let yourArray = [
-  [
-    {
-      one: 1,
-      two: 2
-    },
-    {
-      three: 3,
-      four: 4
-    }
-  ],
-  [
-    {
-      a: "a",
-      b: "b"
-    },
-    {
-      c: "c",
-      d: "d"
-    }
-  ]
-];
-console.log(yourArray.length);
-console.log(yourArray);
+let yourArray = ['Im Dhanvanti',22,true,'Welcome',2012];
+
